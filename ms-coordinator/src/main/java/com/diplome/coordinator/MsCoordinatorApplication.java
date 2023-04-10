0aef6cd91acf334582e1e3cd92fdc25774fe8dd0
Igli Balla <igliballa312@gmail.com>
2023-04-10 21:10:11 +0200

    Initial Commit

---
+++ ms-coordinator/src/main/java/com/diplome/coordinator/MsCoordinatorApplication.java
@@ -0,0 +1,13 @@
+package com.diplome.coordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCoordinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCoordinatorApplication.class, args);
+    }
+
+}
---
+++ ms-coordinator/src/test/java/com/diplome/coordinator/MsCoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplome.coordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCoordinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
