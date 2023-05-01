cd52e8986d7727b4890e5026b2b1efd5f0aeaa9e
Igli Balla <igliballa312@gmail.com>
2023-05-01 20:11:53 +0200

    Modules added

---
+++ ms-extractor/src/main/java/com/diplome/extractor/MsExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.diplome.extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsExtractorApplication.class, args);
+    }
+
+}
---
+++ ms-extractor/src/test/java/com/diplome/extractor/MsExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplome.extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsExtractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
